e04776871af988ddda651fed8f40954f2d2335f3
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-11-04 13:31:20 +0700

    refactor: product and product list/grid views structure

---
+++ Assets/_Scripts/ProductItemView.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ProductItemView : MonoBehaviour
+{
+    [SerializeField] protected TMP_Text productName;
+    [SerializeField] protected TMP_Text productDescription;
+    [SerializeField] protected TMP_Text productPrice;
+    [SerializeField] protected Button initialIncreaseButton;
+    [SerializeField] protected Button increaseButton;
+    [SerializeField] protected Button decreaseButton;
+    [SerializeField] protected GameObject quantityAdjustmentGroup;
+
+    protected virtual void Start()
+    {
+        initialIncreaseButton.onClick.AddListener(AddFirstItem);
+    }
+
+    protected void AddFirstItem()
+    {
+        initialIncreaseButton.gameObject.SetActive(false);
+
+        // No need?
+        // foreach (var image in initialIncreaseButton.GetComponents<Image>())
+        // {
+        //     image.enabled = false;
+        // }
+
+        quantityAdjustmentGroup.SetActive(true);
+    }
+}
