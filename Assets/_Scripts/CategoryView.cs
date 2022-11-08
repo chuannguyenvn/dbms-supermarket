3307be37103095c3e93485df55bd5bb42ac6680f
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-11-08 13:35:50 +0700

    feat: scroll reset when changing important views

--- Assets/_Scripts/CategoryView.cs
+++ Assets/_Scripts/CategoryView.cs
@@ -2,13 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CategoryView : UIElement
+public class CategoryView : ScrollElement
 {
-    [SerializeField] private ScrollRect scrollRect;
 
-    protected override void ScreenChangedHandler()
-    {
-        base.ScreenChangedHandler();
-        scrollRect.content.anchoredPosition = new Vector2(0, scrollRect.content.anchoredPosition.y);
-    }
 }
---
+++ Assets/_Scripts/ScrollElement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollElement : UIElement
+{
+    [SerializeField] private ScrollRect scrollRect;
+
+    protected override void ScreenChangedHandler()
+    {
+        base.ScreenChangedHandler();
+        scrollRect.content.anchoredPosition = new Vector2(0, 0);
+    }
+}
