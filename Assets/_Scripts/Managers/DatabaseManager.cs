f220c2137c5224bbd85bb9954dc11d8fef495a57
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-11-30 11:43:03 +0700

    feat: skeleton code for interacting with database

---
+++ Assets/_Scripts/Managers/DatabaseManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DatabaseManager : PersistentSingleton<DatabaseManager>
+{
+    private void Start()
+    {
+        // Code to connect to database.
+    }
+
+    private List<List<string>> ExecuteQuery(string query)
+    {
+        List<List<string>> queryResult = new();
+
+        // Code to execute query and get the result.
+
+        return queryResult;
+    }
+
+    /// <returns>ID, First_name, Last_name, Gender, Address, Phone_number, Date_of_birth, Type</returns>
+    public List<string> GetCustomer(string ID)
+    {
+        // Delete this //
+        return new List<string>()
+        {
+            "69420",
+            "A",
+            "Nguyen",
+            "Male",
+            "495/4/8 To Hien Thanh, D.10, HCM City",
+            "0123696969",
+            "1969/6/9",
+            "Type?",
+        };
+        // Delete this //
+
+        string query = "[SQL code here!]";
+        return ExecuteQuery("query")[0];
+    }
+    
+    /// <returns>Customer_ID, Issue_date, Saving_points</returns>
+    public List<string> GetCustomerCard(string Customer_ID)
+    {
+        // Delete this //
+        return new List<string>() {"6090609", "2022/6/9", "1696",};
+        // Delete this //
+
+        string query = "[SQL code here!]";
+        return ExecuteQuery("query")[0];
+    }
+}
--- Assets/_Scripts/UIs/CustomerCard.cs
+++ Assets/_Scripts/UIs/CustomerCard.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,6 +8,7 @@ public class CustomerCard : Singleton<CustomerCard>
 {
     [SerializeField] private TMP_Text cardTypeText;
     [SerializeField] private TMP_Text pointText;
+    [SerializeField] private TMP_Text issueDate;
     
     private int pointCount;
     
@@ -18,5 +20,13 @@ public class CustomerCard : Singleton<CustomerCard>
             pointCount = value;
             pointText.text = pointCount.ToString("{0:n0}");
         }
+    }
+
+    private void Start()
+    {
+        var cardInformation = DatabaseManager.Instance.GetCustomerCard("lmao");
+
+        PointCount = int.Parse(cardInformation[2]);
+        issueDate.text = cardInformation[1];
     }
 }
