4c3d694073197284781cd17c394f60ae728535c8
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-12-12 11:38:02 +0700

    feat: good item view

---
+++ Assets/GoodGridItemView.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GoodGridItemView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text nameText;
+    [SerializeField] private TMP_Text priceText;
+    [SerializeField] private TMP_Text brandText;
+    [SerializeField] private TMP_Text typeText;
+}
