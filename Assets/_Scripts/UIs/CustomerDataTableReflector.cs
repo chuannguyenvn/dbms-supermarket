cbbfa60861da2621dd795b81f89df2db75f36c10
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-12-13 23:11:43 +0700

    refactor: TableReflectors

--- Assets/_Scripts/UIs/CustomerDataTableReflector.cs
+++ Assets/_Scripts/UIs/CustomerDataTableReflector.cs
@@ -3,58 +3,49 @@
 
 public class CustomerDataTableReflector : TableReflector<CustomerData>
 {
-    // DEBUG //
-
-    protected override void Start()
-    {
-        base.Start();
-        Init(DapperFacade.Instance.QuerySingle<CustomerData>(Function.ReadCustomer, new {ID = 1}));
-    }
-
-    // DEBUG //
+    private Field firstNameField;
+    private Field lastNameField;
+    private Field genderField;
+    private Field addressField;
+    private Field phoneNumberField;
+    private Field dateOfBirthField;
+    private Field typeField;
 
 
     public override void Init(CustomerData data)
     {
-        var editableTextPrefab = ResourceManager.Instance.EditableText;
+        this.data = data;
+        CreateFields();
+        UpdateButtonsSiblingIndex();
+    }
 
-        var firstNameEditableText = Instantiate(editableTextPrefab, transform).GetComponent<EditableText>();
-        firstNameEditableText.Init("First_name", "First name", data.First_name);
-        AddNewEditableText(firstNameEditableText);
-
-        var lastNameEditableText = Instantiate(editableTextPrefab, transform).GetComponent<EditableText>();
-        lastNameEditableText.Init("Last_name", "Last name", data.Last_name);
-        AddNewEditableText(lastNameEditableText);
-
-        var genderEditableText = Instantiate(editableTextPrefab, transform).GetComponent<EditableText>();
-        genderEditableText.Init("Gender", "Gender", data.Gender);
-        AddNewEditableText(genderEditableText);
-
-        var addressEditableText = Instantiate(editableTextPrefab, transform).GetComponent<EditableText>();
-        addressEditableText.Init("Address", "Address", data.Address);
-        AddNewEditableText(addressEditableText);
-
-        var phoneNumberEditableText = Instantiate(editableTextPrefab, transform).GetComponent<EditableText>();
-        phoneNumberEditableText.Init("Phone_number", "Phone number", data.Phone_number);
-        AddNewEditableText(phoneNumberEditableText);
-
-        var dateOfBirthEditableText = Instantiate(editableTextPrefab, transform).GetComponent<EditableText>();
-        dateOfBirthEditableText.Init("Date_of_birth", "Date of birth", data.Date_of_birth.ToString());
-        AddNewEditableText(dateOfBirthEditableText);
-
-        var typeEditableText = Instantiate(editableTextPrefab, transform).GetComponent<EditableText>();
-        typeEditableText.Init("Type", "Type", data.Type);
-        AddNewEditableText(typeEditableText);
-
-        var childCount = transform.childCount;
-        editButton.transform.SetSiblingIndex(childCount - 1);
-        saveButton.transform.SetSiblingIndex(childCount - 1);
-        cancelButton.transform.SetSiblingIndex(childCount - 1);
+    protected override void CreateFields()
+    {
+        firstNameField = CreateNewField("First_name", "First name", data.First_name, true);
+        lastNameField = CreateNewField("Last_name", "Last name", data.Last_name, true);
+        genderField = CreateNewField("Gender", "Gender", data.Gender, true);
+        addressField = CreateNewField("Address", "Address", data.Address, true);
+        phoneNumberField = CreateNewField("Phone_number", "Phone number", data.Phone_number, true);
+        dateOfBirthField =
+            CreateNewField("Date_of_birth", "Date of birth", data.Date_of_birth.Value, true);
+        typeField = CreateNewField("Type", "Type", data.Type, false);
     }
 
     protected override void Save()
     {
         base.Save();
-        
+
+        DapperFacade.Instance.QueryNone(Function.UpdateCustomer,
+            new
+            {
+                ID = data.ID,
+                First_name = firstNameField.Value,
+                Last_name = lastNameField.Value,
+                Gender = genderField.Value,
+                Address = addressField.Value,
+                Phone_number = phoneNumberField.Value,
+                Date_of_birth = dateOfBirthField.Value,
+                Type = data.Type,
+            });
     }
 }
--- Assets/_Scripts/UIs/TableReflector.cs
+++ Assets/_Scripts/UIs/TableReflector.cs
@@ -5,47 +5,78 @@ using UnityEngine.UI;
 
 public abstract class TableReflector<T> : MonoBehaviour
 {
+    protected T data;
+
     [SerializeField] protected Button editButton;
     [SerializeField] protected Button saveButton;
     [SerializeField] protected Button cancelButton;
-    protected List<EditableText> editableTexts = new();
-    
-    
+    protected List<Field> editableTexts = new();
+
+
     protected virtual void Start()
     {
         editButton.onClick.AddListener(Edit);
         saveButton.onClick.AddListener(Save);
         cancelButton.onClick.AddListener(Cancel);
-        
+
         View();
     }
 
     public abstract void Init(T data);
+    protected abstract void CreateFields();
 
-    protected void AddNewEditableText(EditableText editableText)
+    protected void AddNewEditableField(Field field)
     {
-        editableTexts.Add(editableText);
+        editableTexts.Add(field);
     }
-    
+
+    protected Field CreateNewField(string fieldDatabaseName, string fieldDisplayName, string value,
+        bool editable)
+    {
+        var prefab = ResourceManager.Instance.field;
+        var field = Instantiate(prefab, transform).GetComponent<Field>();
+        field.Init(fieldDatabaseName, fieldDisplayName, value, editable);
+        if (editable) AddNewEditableField(field);
+        return field;
+    }
+
+    protected Field CreateNewField(string fieldDatabaseName, string fieldDisplayName, DateTime value,
+        bool editable)
+    {
+        var prefab = ResourceManager.Instance.field;
+        var field = Instantiate(prefab, transform).GetComponent<Field>();
+        field.Init(fieldDatabaseName, fieldDisplayName, value, editable);
+        if (editable) AddNewEditableField(field);
+        return field;
+    }
+
+    protected void UpdateButtonsSiblingIndex()
+    {
+        var childCount = transform.childCount;
+        editButton.transform.SetSiblingIndex(childCount - 1);
+        saveButton.transform.SetSiblingIndex(childCount - 1);
+        cancelButton.transform.SetSiblingIndex(childCount - 1);
+    }
+
     protected virtual void View()
     {
         foreach (var editableText in editableTexts)
         {
             editableText.View();
         }
-        
+
         editButton.gameObject.SetActive(true);
         saveButton.gameObject.SetActive(false);
         cancelButton.gameObject.SetActive(false);
     }
-    
+
     protected virtual void Edit()
     {
         foreach (var editableText in editableTexts)
         {
             editableText.Edit();
         }
-        
+
         editButton.gameObject.SetActive(false);
         saveButton.gameObject.SetActive(true);
         cancelButton.gameObject.SetActive(true);
@@ -57,7 +88,7 @@ public abstract class TableReflector<T> : MonoBehaviour
         {
             editableText.Save();
         }
-        
+
         View();
     }
 
@@ -67,7 +98,7 @@ public abstract class TableReflector<T> : MonoBehaviour
         {
             editableText.Cancel();
         }
-        
+
         View();
     }
 }
