5102aaf3a0ccd9c2ed66e711f2973b676e5ebb55
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-12-13 23:09:51 +0700

    feat: field functions

--- Assets/_Scripts/UIs/Field.cs
+++ Assets/_Scripts/UIs/Field.cs
@@ -4,20 +4,32 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class EditableText : MonoBehaviour
+public class Field : MonoBehaviour
 {
     [HideInInspector] public string FieldDatabaseName;
     [HideInInspector] public string FieldDisplayName;
     [SerializeField] private TMP_Text fieldDisplayNameText;
     [SerializeField] private TMP_Text viewText;
     [SerializeField] private TMP_InputField editField;
+    private bool editable;
+    public string Value => viewText.text;
 
-
-    public void Init(string fieldDatabaseName, string fieldDisplayName, string value)
+    public void Init(string fieldDatabaseName, string fieldDisplayName, string value, bool editable)
     {
         FieldDatabaseName = fieldDatabaseName;
         fieldDisplayNameText.text = FieldDisplayName = fieldDisplayName;
         viewText.text = editField.text = value;
+        this.editable = editable;
+
+        View();
+    }
+
+    public void Init(string fieldDatabaseName, string fieldDisplayName, DateTime value, bool editable)
+    {
+        FieldDatabaseName = fieldDatabaseName;
+        fieldDisplayNameText.text = FieldDisplayName = fieldDisplayName;
+        viewText.text = editField.text = value.ToString("yyyyMMdd");
+        this.editable = editable;
 
         View();
     }
